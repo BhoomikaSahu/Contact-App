a0f28e4160be8b876600c68e634c09fb61ac5cf1
Prateek <shukla.prateek.tftuf.com>
2022-03-25 18:34:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { AddContact } from "./Component/AddContact/AddContact";
+import { DisplayPage } from "./Component/DisplayContact/DisplayPage";
+import { Editcontact } from "./Component/EditContact/Editcontact";
+import { Header } from "./Component/UI/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/Add-contact" element={<AddContact />} />
+        <Route path="/displayPage" element={<DisplayPage />} />
+        <Route path="/editcontact" element={<Editcontact />} />
+        <Route path="/" element={<AddContact />} />
+        <Route path="*" element={<AddContact />} />
+      </Routes>
     </div>
   );
 }
